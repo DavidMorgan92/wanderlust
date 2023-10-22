59cdb1e49d3366d7d3d545a1f85600dcf5062b52
David Morgan <david.r.morgan.92@gmail.com>
2023-10-22 14:53:52 +0100

    Contact page

---
+++ assets/js/contact.js
@@ -0,0 +1,31 @@
+const publicKey = "UafpwJjcuUvnRA4-m";
+const serviceId = "wanderlust";
+const templateId = "template_9dhd7zl";
+
+$(function () {
+    // Init emailJS
+    emailjs.init(publicKey);
+
+    // Respond to contact form submit events
+    $("#contact-form").on("submit", async function (event) {
+        // Prevent page reload
+        event.preventDefault();
+
+        try {
+            // Send email
+            await emailjs.sendForm(serviceId, templateId, "#contact-form");
+
+            console.log("Successfully sent email");
+
+            // Show success alert and hide error alert
+            $(".alert-success").attr("hidden", false);
+            $(".alert-danger").attr("hidden", true);
+        } catch (error) {
+            console.log("Failed to send email", error);
+
+            // Show error alert and hide success alert
+            $(".alert-success").attr("hidden", true);
+            $(".alert-danger").attr("hidden", false);
+        }
+    });
+});
